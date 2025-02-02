181c82e958d74e85673c9093439e1e55a39c37fd
oriol <oriolgfarssac@gmail.com>
2025-02-02 20:24:46 +0100

    act3 with docker created, but not all act done

--- ACTIVITAT_13/TIC_BCN_OF/TIC_BCN_OF/settings.py
+++ ACTIVITAT_13/TIC_BCN_OF/TIC_BCN_OF/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'TIC_BCN_OF.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgradesql',
+        'NAME': "ITIC_DAW",
+        "USER":"oriol",
+        "PASSWORD": "oriol123",
+        "HOST": "db",
+        "PORT": "5432",
     }
 }
 
--- ACTIVITAT_13/TIC_BCN_OF/centre/models.py
+++ ACTIVITAT_13/TIC_BCN_OF/centre/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Persona(models.Model):
+    nom = models.CharField(max_length=100)
+    cognom = models.CharField(max_length=100)
+    edat = models.IntegerField()
+    rol = models.CharField(max_length=50)
+    curs = models.CharField(max_length=50)
+    data_creacio = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.nom
