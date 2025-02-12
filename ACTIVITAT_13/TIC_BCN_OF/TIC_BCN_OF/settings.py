e8ef57a42c52b6b3afa018508d124cbcd586a596
oriolgfarssac <oriolgfarssac@gmail.com>
2025-02-12 20:02:33 +0100

    poc a pooc

--- ACTIVITAT_13/TIC_BCN_OF/TIC_BCN_OF/settings.py
+++ ACTIVITAT_13/TIC_BCN_OF/TIC_BCN_OF/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'centre.apps.CentreConfig',
+    'authapp'
 ]
 
 MIDDLEWARE = [
---
+++ ACTIVITAT_13/TIC_BCN_OF/authapp/models.py
@@ -0,0 +1,5 @@
+from django.contrib.auth.models import AbstractUser
+
+class CustomUser(AbstractUser):
+    # Puedes añadir campos adicionales si lo necesitas
+    pass
